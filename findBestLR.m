9bacea124c35e524ee7a37e38fccfb3c7cd9a442
filippos1994 <phkonidaris@gmail.com>
2016-12-04 22:25:47 +0200

    Finds best learning rate. Used in 6e

---
+++ findBestLR.m
@@ -0,0 +1,12 @@
+function [BestLR BestLRTraining] = findBestLR(meanaccuracyRate)
+
+index_row = find(max(max(meanaccuracyRate'))==max(meanaccuracyRate'));
+index_column = find(max(max(meanaccuracyRate))==max(meanaccuracyRate));
+
+if (index_column == 1)
+    BestLR = index_row * 0.05;
+    BestLRTraining = 'traingd';
+else
+    BestLR = index_row * 0.005;
+    BestLRTraining = 'traingdx';
+end
